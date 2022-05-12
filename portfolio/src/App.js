69c86200bfd8f68fc227712a5a0b6aab5193e312
chandramouli yerraguntla <chandramouli@chandramoulis-MacBook-Pro.local>
2022-05-12 12:11:08 +0530

    Chandramouli | Add . Initial files

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>This guy is so lazy to build his portflio,but trust me he is a very good developer.</p>
+      <p>He loves Challeneges,Money and food.If you've all these consider hiring him</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
